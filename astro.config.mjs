cfbdb522280ce5368eeb993175c8df36fa77e5c4
Neil de Carteret <git@lumphammer.net>
2026-02-06 20:02:35 +0000

    add cf adapter (v5)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import { defineConfig } from "astro/config";
 
 import node from "@astrojs/node";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
 
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: cloudflare(),
 });
